983fb7e0c96d92a8b9d3e15e4a76502140464ff6
kandriy <andriy.karpenko@mediaux.biz>
2015-10-20 18:46:02 +0300

    add login view

--- app/src/main/java/com/miami/moveforless/activity/BaseActivity.java
+++ app/src/main/java/com/miami/moveforless/activity/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.miami.moveforless.activity;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by SetKrul on 14.07.2015.
@@ -12,5 +15,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     @SuppressWarnings("unchecked")
     public final <T extends View> T findView(@IdRes int id) {
         return (T) findViewById(id);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/miami/moveforless/activity/LoginActivity.java
+++ app/src/main/java/com/miami/moveforless/activity/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.miami.moveforless.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.miami.moveforless.R;
+
+import butterknife.Bind;
 
 /**
  * Created by klim on 20.10.15.
  */
 public class LoginActivity extends BaseActivity {
+    @Bind(R.id.et_email_AL)         EditText etEmail;
+
+    @Bind(R.id.et_password_AL)      EditText etPassword;
+
+    @Bind(R.id.btn_login_AL)        Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
