57a3c0d97b919cb853eb35fc38cdf00b306587a1
kandriy <andriy.karpenko@mediaux.biz>
2015-10-26 19:31:02 +0200

    add base dialog, signature dialog

---
+++ app/src/main/java/com/miami/moveforless/SignatureDialog.java
@@ -0,0 +1,68 @@
+package com.miami.moveforless;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.gesture.GestureOverlayView;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+
+import com.jakewharton.rxbinding.view.RxView;
+import com.miami.moveforless.fragments.BaseDialog;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import butterknife.Bind;
+
+/**
+ * Created by klim on 26.10.15.
+ */
+public class SignatureDialog extends BaseDialog {
+    public static final int REQUEST_CODE = 1000;
+    public static final String SIGNATURE_BITMAP_KEY = "SIGNATURE_BITMAP_KEY";
+
+    @Bind(R.id.gesture_SL)
+    GestureOverlayView mGestureView;
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.signature_layout;
+    }
+
+    @Override
+    protected void setupViews() {
+        setTitle(getString(R.string.signature_title));
+        setDescription(getString(R.string.signature_description));
+        setPositiveTitle(getString(R.string.ok));
+        setNegativeTitle(getString(R.string.clear));
+        setCancelable(true);
+
+    }
+
+    @Override
+    public void onNegativeClicked() {
+        mGestureView.clear(true);
+    }
+
+    @Override
+    public void onPositiveClicked() {
+        try {
+
+            mGestureView.setDrawingCacheEnabled(true);
+            Bitmap bm = Bitmap.createBitmap(mGestureView.getDrawingCache());
+            Intent data = new Intent();
+            data.putExtra(SIGNATURE_BITMAP_KEY, bm);
+            getTargetFragment().onActivityResult(REQUEST_CODE, Activity.RESULT_OK, data);
+        } catch (Exception e) {
+            getTargetFragment().onActivityResult(REQUEST_CODE, Activity.RESULT_CANCELED, null);
+        }
+
+        super.onPositiveClicked();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/miami/moveforless/fragments/BaseDialog.java
@@ -0,0 +1,110 @@
+package com.miami.moveforless.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding.view.RxView;
+import com.miami.moveforless.R;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by klim on 26.10.15.
+ */
+public abstract class BaseDialog extends DialogFragment {
+    @Bind(R.id.content_container_BDL)
+    FrameLayout mContainerLayout;
+    @Bind(R.id.tvTitle_BDL)
+    TextView tvTitle;
+    @Bind(R.id.tvDescription_BDL)
+    TextView tvDescription;
+    @Bind(R.id.btnPositive_BDL)
+    Button btnPositive;
+    @Bind(R.id.btnNegative_BDL)
+    Button btnNegative;
+
+    private DialogListener positiveOnClick;
+    private DialogListener negativeOnClick;
+
+    private int mContentResource = getLayoutResource();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater _inflater, ViewGroup _container, Bundle _savedInstanceState) {
+        super.onCreateView(_inflater, _container, _savedInstanceState);
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        View rootView = _inflater.inflate(R.layout.base_dialog_layout, _container, false);
+        if (mContentResource != 0) {
+            FrameLayout frameLayout = (FrameLayout) rootView.findViewById(R.id.content_container_BDL);
+            frameLayout.addView(_inflater.inflate(mContentResource, frameLayout, false));
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View _view, Bundle _savedInstanceState) {
+        super.onViewCreated(_view, _savedInstanceState);
+        ButterKnife.bind(this, _view);
+        setupViews();
+        RxView.clicks(btnPositive).throttleFirst(500, TimeUnit.MILLISECONDS).subscribe(o -> onPositiveClicked());
+        RxView.clicks(btnNegative).throttleFirst(500, TimeUnit.MILLISECONDS).subscribe(o -> onNegativeClicked());
+    }
+
+    protected abstract int getLayoutResource();
+    protected abstract void setupViews();
+
+    public void onPositiveClicked() {
+        if (positiveOnClick != null) positiveOnClick.onClick();
+        dismiss();
+    }
+
+    public void onNegativeClicked(){
+        if (negativeOnClick != null) negativeOnClick.onClick();
+        dismiss();
+    }
+
+    public void setPositiveClickListener(DialogListener _listener) {
+        positiveOnClick = _listener;
+    }
+
+    public void setNegativeClickListener(DialogListener _listener) {
+        negativeOnClick = _listener;
+    }
+
+    public void setTitle(String _title) {
+        tvTitle.setVisibility(View.VISIBLE);
+        tvTitle.setText(_title);
+    }
+
+    public void setPositiveTitle(String _title) {
+        btnPositive.setVisibility(View.VISIBLE);
+        btnPositive.setText(_title);
+    }
+
+    public void setNegativeTitle(String _title) {
+        btnNegative.setVisibility(View.VISIBLE);
+        btnNegative.setText(_title);
+    }
+
+    public void setDescription(String _description) {
+        tvDescription.setVisibility(View.VISIBLE);
+        tvDescription.setText(_description);
+    }
+
+    public interface DialogListener {
+        void onClick();
+    }
+}
