8f2dc9a0338894a36b033f6803e53c7d3ffb40ed
SetKrul <setkrul@gmail.com>
2015-11-17 17:10:39 +0200

    add job list? add job details

--- app/src/main/java/com/miami/moveforless/globalconstants/Const.java
+++ app/src/main/java/com/miami/moveforless/globalconstants/Const.java
@@ -1,6 +1,7 @@
 package com.miami.moveforless.globalconstants;
 
 import com.miami.moveforless.fragments.eventbus.FragmentType;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 import static com.miami.moveforless.fragments.eventbus.FragmentType.CLAIM;
 import static com.miami.moveforless.fragments.eventbus.FragmentType.CONFIRMATION;
@@ -39,5 +40,21 @@ public class Const {
     final public static int REQUEST_TYPE_PHOTO = 0;
     final public static int REQUEST_PHOTO = 10000;
     final public static int REQUEST_GALLERY_IMAGE = 10002;
+
+    //pay
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+    //sendbox test
+    private static final String CONFIG_CLIENT_ID =
+            "AekC6Q3O01zbLuDkSQEcbrCawwXXo5Hzi68OyHGHVcRU2D6neCcvwO6wrZli7YNpFvC55ZfT4zFcc_UA";
+    //real test
+//    private static final String CONFIG_CLIENT_ID = "AVWOzMxGpnIUKb6Zz-nQQUIixuEwcEkHQVuGaSLJpY0kd3vwLmd2fDQYh8KqgkCpjGnnopk_YG1zW5ia";
+
+
+    public static final int REQUEST_CODE_PAYMENT = 1;
+    public static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    public static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
+    public static PayPalConfiguration config = new PayPalConfiguration().environment(CONFIG_ENVIRONMENT).clientId
+            (CONFIG_CLIENT_ID).rememberUser(false);
 
 }
